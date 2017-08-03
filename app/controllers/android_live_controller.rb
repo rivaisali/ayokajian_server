e03cfba9097b4adbde273d523d14e640701329b3
Mohammad Rivai Sali <m.rivai.sali@gmail.com>
2017-08-03 12:59:10 +0700

    Update android_live_controller.rb

--- app/controllers/android_live_controller.rb
+++ app/controllers/android_live_controller.rb
@@ -1,6 +1,6 @@
 class AndroidLiveController < AndroidAbstractController
 
-  PRD_SERVER_HOST_ADDRESS = 'https://ec2-52-78-228-123.ap-northeast-2.compute.amazonaws.com'
+  PRD_SERVER_HOST_ADDRESS = 'https://api.cloud.wowza.com'
   DEV_SERVER_HOST_ADDRESS = 'http://localhost'
 
   def create
